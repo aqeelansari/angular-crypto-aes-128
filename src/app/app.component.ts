de91e2679f5ab42f41f0f81de5a6aa206814f032
aqeelansari <aqeelansari@live.com>
2022-04-18 18:10:21 +0530

    Added Cipher Key and IV

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
-
 import { Component } from '@angular/core';
 import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  tokenFromUI: string = "0123456789123456";
-  encrypted: any = "";
+  tokenFromUI: string = 'b7ab81c783da4045ba3655e5cedda314';
+  iVFromUI: string = '9adccc6ad6ad4c79b86406e8b3374779';
+  encrypted: any = '';
   decrypted: string;
 
   request: string;
@@ -19,26 +19,24 @@ export class AppComponent {
   }
   encryptUsingAES256() {
     let _key = CryptoJS.enc.Utf8.parse(this.tokenFromUI);
-    let _iv = CryptoJS.enc.Utf8.parse(this.tokenFromUI);
-    let encrypted = CryptoJS.AES.encrypt(
-      JSON.stringify(this.request), _key, {
-        keySize: 128,
-        iv: _iv,
-        mode: CryptoJS.mode.ECB,
-        padding: CryptoJS.pad.Pkcs7
-      });
+    let _iv = CryptoJS.enc.Utf8.parse(this.iVFromUI);
+    let encrypted = CryptoJS.AES.encrypt(JSON.stringify(this.request), _key, {
+      keySize: 128,
+      iv: _iv,
+      mode: CryptoJS.mode.ECB,
+      padding: CryptoJS.pad.Pkcs7,
+    });
     this.encrypted = encrypted.toString();
   }
   decryptUsingAES256() {
     let _key = CryptoJS.enc.Utf8.parse(this.tokenFromUI);
-    let _iv = CryptoJS.enc.Utf8.parse(this.tokenFromUI);
+    let _iv = CryptoJS.enc.Utf8.parse(this.iVFromUI);
 
-    this.decrypted = CryptoJS.AES.decrypt(
-      this.encrypted, _key, {
-        keySize: 128,
-        iv: _iv,
-        mode: CryptoJS.mode.ECB,
-        padding: CryptoJS.pad.Pkcs7
-      }).toString(CryptoJS.enc.Utf8);
+    this.decrypted = CryptoJS.AES.decrypt(this.encrypted, _key, {
+      keySize: 128,
+      iv: _iv,
+      mode: CryptoJS.mode.ECB,
+      padding: CryptoJS.pad.Pkcs7,
+    }).toString(CryptoJS.enc.Utf8);
   }
 }
